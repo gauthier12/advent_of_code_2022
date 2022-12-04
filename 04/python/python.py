d6f821c3e98dc504a1f59c7f63e7c5b1ccb139cd
Gauthier Folzan <gauthier@wuhu.eu>
2022-12-04 16:54:31 +0100

    Python day 4

---
+++ 04/python/python.py
@@ -0,0 +1,30 @@
+from parse import *
+import time
+import sys
+start = time.time()
+filename = sys.argv[1]
+print("===========================")
+print("  Advent Of Code 2022")
+print("  Day 04")
+print("  Python version")
+print("===========================")
+print("In file {}".format(filename))
+num_pair_a = 0
+num_pair_b = 0
+with open(filename, "r") as f:
+    for line in f:
+        parsed_line = parse("{}-{},{}-{}\n", line)
+        min_1 = int(parsed_line[0])
+        max_1 = int(parsed_line[1])
+        min_2 = int(parsed_line[2])
+        max_2 = int(parsed_line[3])
+        # an interval is included if the min and the max is inside the other interval
+        if (min_1>=min_2 and max_1<=max_2) or (min_2>=min_1 and max_2<=max_1):
+            num_pair_a = num_pair_a + 1
+        # if min AND max of an interval are under the min of the other, it doesn't overlap --> if not, it overlaps
+        if not(((min_1 < min_2) and (max_1 < min_2)) or ((min_2 < min_1) and (max_2 < min_1))):
+            num_pair_b += 1
+end = time.time()
+print("Time elapsed in total is: {}ms".format((end - start)*1000));
+print("Problem A : number of included pair : {}".format(num_pair_a))
+print("Problem B : number of overlapping pair : {}".format(num_pair_b))

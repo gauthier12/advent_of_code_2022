bd51c8614966f6417c1a94a89addb90c684e3ce2
Gauthier Folzan <gauthier@wuhu.eu>
2022-12-05 23:51:24 +0100

    Performance tweak

--- 05/python/python.py
+++ 05/python/python.py
@@ -1,4 +1,5 @@
-from parse import *
+#from parse import *
+import re
 import time
 import sys
 import copy
@@ -12,6 +13,7 @@ print("===========================")
 print("In file {}".format(filename))
 crate_piles = []
 instructions = []
+prog = re.compile('[0-9]+')
 with open(filename, "r") as f:
     for line in f:
         if "[" in line:
@@ -23,10 +25,12 @@ with open(filename, "r") as f:
                 crate_piles[i_crate].append(line[4*i_crate+1])
         if "move" in line:
             # this is a move instruction
-            parsed = parse("move {number:d} from {from:d} to {to:d}\n", line)
+            #parsed = parse("move {number:d} from {from:d} to {to:d}\n", line)
+            # instructions.append(
+            #    {"number": parsed["number"], "from": parsed["from"]-1, "to": parsed["to"]-1})
+            parsed2 = prog.findall(line)
             instructions.append(
-                {"number": parsed["number"], "from": parsed["from"]-1, "to": parsed["to"]-1})
-
+                (int(parsed2[0]), int(parsed2[1])-1, int(parsed2[2])-1))
     # Put the pile in the right order for pushing/popping
     for pile in crate_piles:
         pile.reverse()
@@ -35,32 +39,22 @@ with open(filename, "r") as f:
 
     # Copy the initial state and solve problem A
     crate_piles_a = copy.deepcopy(crate_piles)
-    for inst in instructions:
-        for i_crate in range(0, inst["number"]):
+    for ins_number, ins_from, ins_to in instructions:
+        for i_crate in range(0, ins_number):
             # move the crate one by one
-            cur_crate = crate_piles_a[inst["from"]].pop()
-            crate_piles_a[inst["to"]].append(cur_crate)
-    message_a = ""
-    for pile in crate_piles_a:
-        message_a += pile.pop()
+            crate_piles_a[ins_to].append(crate_piles_a[ins_from].pop())
+    message_a = ''.join([str(pile[-1]) for pile in crate_piles_a])
 
     # Copy the initial state and solve problem B
     crate_piles_b = copy.deepcopy(crate_piles)
-    for inst in instructions:
+    for ins_number, ins_from, ins_to in instructions:
         # move the crate on the loaded crate pile
-        loaded_crate_pile = []
-        for i_crate in range(0, inst["number"]):
-            # move the crate one by one
-            cur_crate = crate_piles_b[inst["from"]].pop()
-            loaded_crate_pile.append(cur_crate)
+        long = len(crate_piles_b[ins_from])
+        loaded_crate_pile = crate_piles_b[ins_from][long-ins_number:long]
+        del crate_piles_b[ins_from][long-ins_number:long]
         # unload the loaded crate pile
-        for i_crate in range(0, inst["number"]):
-            # move the crate one by one
-            cur_crate = loaded_crate_pile.pop()
-            crate_piles_b[inst["to"]].append(cur_crate)
-    message_b = ""
-    for pile in crate_piles_b:
-        message_b += pile.pop()
+        crate_piles_b[ins_to].extend(loaded_crate_pile)
+    message_b = ''.join([str(pile[-1]) for pile in crate_piles_b])
 end = time.time()
 print("Time elapsed in total is: {}ms".format((end - start)*1000))
 print("Problem A : message : {}".format(message_a))
